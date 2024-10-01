f5dc8355ef82fdddff582670f1602f9d18315615
dharshanaks005 <dharshanaks005@gmail.com>
2024-10-01 18:38:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Optional: Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
